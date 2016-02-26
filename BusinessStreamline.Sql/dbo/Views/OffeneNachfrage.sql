95d5ff1b0259d463c73ee38285176e7797d1018e
NightkingCH <thomas.erni@nk-s.ch>
2016-02-26 18:51:40 +0100

    Add views to check for open requests

---
+++ BusinessStreamline.Sql/dbo/Views/OffeneNachfrage.sql
@@ -0,0 +1,8 @@
+﻿CREATE VIEW dbo.OffeneNachfrage
+	AS
+SELECT 
+	n.NachfrageId
+	, n.TeilId
+FROM dbo.Nachfrage n
+	LEFT JOIN dbo.Bestellung b ON b.NachfrageId = n.NachfrageId
+WHERE b.BestellungId IS NULL
---
+++ BusinessStreamline.Sql/dbo/Views/Teile.sql
@@ -0,0 +1,17 @@
+﻿CREATE VIEW dbo.Teile
+	AS
+SELECT
+	t.TeilId
+	, t.Name AS TeilName
+	, t.Anzahl
+	, tt.TypId
+	, tt.Name AS TypName
+	, p.ProduktId
+	, p.Name AS ProduktName
+	, CASE WHEN EXISTS (SELECT TOP 1 n.NachfrageId FROM dbo.OffeneNachfrage n WHERE n.TeilId = t.TeilId)
+		THEN CAST(1 AS BIT)
+		ELSE CAST(0 AS BIT)
+	END AS HatOffeneNachfrage
+FROM dbo.Teil t
+	INNER JOIN dbo.Produkt p ON p.ProduktId = t.ProduktId
+	INNER JOIN dbo.Typ tt ON tt.TypId = t.TypId
