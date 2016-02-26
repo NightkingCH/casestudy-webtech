dfdf3b0cc7231c5fd7c899d4e98a532f142ab73f
NightkingCH <thomas.erni@nk-s.ch>
2016-02-26 15:26:26 +0100

    Add search view for proper references

---
+++ BusinessStreamline.Sql/dbo/Views/SucheNachfrage.sql
@@ -0,0 +1,16 @@
+﻿CREATE VIEW dbo.SucheNachfrage
+	AS
+SELECT 
+	n.NachfrageId
+	, n.Anzahl
+	, n.ErstelltAm
+	, t.TeilId
+	, t.Name AS TeilName
+	, tt.TypId
+	, tt.Name AS TypName
+	, p.ProduktId
+	, p.Name AS ProduktName
+FROM dbo.Nachfrage n
+	INNER JOIN dbo.Teil t ON t.TeilId = n.TeilId
+	INNER JOIN dbo.Typ tt ON tt.TypId = t.TypId
+	INNER JOIN dbo.Produkt p ON p.ProduktId = t.ProduktId
