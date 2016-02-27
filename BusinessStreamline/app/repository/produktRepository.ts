c61d0e5ec47c6570b149577df3e131fd9d0a5fc2
Thomas Erni <thomas.erni@nk-s.ch>
2016-02-27 17:49:10 +0100

    Add site to add a product. Change layout to 90%. Change the layout to use more of provided space :).

--- BusinessStreamline/app/repository/produktRepository.ts
+++ BusinessStreamline/app/repository/produktRepository.ts
@@ -30,6 +30,30 @@ export class ProduktRepository extends Repository {
         return queryPromise;
     }
 
+    public post(model: Produkt): Promise<Produkt> {
+
+        var callConfiguration: RequestInit = {
+            method: "post",
+            headers: {
+                'Accept': "application/json",
+                'Content-Type': "application/json"
+            },
+            body: JSON.stringify(model)
+        };
+
+        var callUri = this.serviceConfig;
+
+        var queryPromise = window.fetch(callUri, callConfiguration)
+            .then(this.parseResponse)
+            .then(this.parseText)
+            .then(this.parseJson)
+            .then((data: Produkt) => {
+                return data;
+            });
+
+        return queryPromise;
+    }
+
     public getByFirma(firma: number, login: number): Promise<Array<Produkt>> {
         var callConfiguration: RequestInit = {
             method: "get",
--- BusinessStreamline/app/sites/produkt/add/add.ts
+++ BusinessStreamline/app/sites/produkt/add/add.ts
@@ -1,14 +1,47 @@
 ﻿import { Component } from 'angular2/core';
 import { COMMON_DIRECTIVES } from 'angular2/common';
+import { Router, RouteParams } from 'angular2/router';
 import { Title } from 'angular2/platform/browser';
+
+declare var $: JQueryStatic;
+
+import { PIPES } from '../../../pipes/pipes';
+
+import { UserService } from '../../../services/services';
+
+import { ProduktRepository } from '../../../repository/repository';
+import { Produkt } from '../../../models/models';
 
 @Component({
     selector: '[data-site-add-produkt]',
     templateUrl: 'app/sites/produkt/add/add.html',
-    directives: [COMMON_DIRECTIVES]
+    directives: [COMMON_DIRECTIVES],
+    pipes: [PIPES]
 })
 export class ProduktAddComponent {
-    constructor(private title: Title) {
+
+    private model: Produkt = new Produkt();
+
+    private repository: ProduktRepository = new ProduktRepository();
+
+    constructor(private router: Router,  private title: Title, private userService: UserService) {
         this.title.setTitle("Produkt | Hinzufügen - BLS");
+    }
+
+    public ngOnInit(): void {
+    }
+
+    public onAdd(event: MouseEvent): void {
+
+        // TODO: add when user service is available.
+        //if (this.userService.isFirma()) {
+        //    return; // companies can't create an offer.
+        //}
+
+        this.model.firmaId = 4; // TODO: Add proper user! => this.userService.firma.firmaId;
+
+        this.repository.post(this.model).then((entity: Produkt) => {
+            this.router.navigateByUrl("/produkt/" + entity.produktId);
+        });
     }
 }
